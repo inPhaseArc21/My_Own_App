4aaa5788115794c7982d2b3ad6632162a4a3119e
Raj Silari <raj.silari@icloud.com>
2016-01-19 18:24:58 +1000

    Create new, edit, index and destroy methods and pages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,16 +4,53 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+
+	def show 
+	    @article = Article.find(params[:id])
+	end
+
+	def index
+		@article = Article.all
+	end
+
 
 	def create
 		@article = Article.new(article_params)
+		if @article.save 
+			flash[:notice] = "Article was successfuly created" 
+			redirect_to article_path(@article)
+		else
+			render 'new'
+	    end
+
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfuly updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end 
+	end
+
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+		flash[:notice] = "Article was successfuly destroyed"
 	end
 
 
 	private
 	def article_params
 		params.require(:article).permit(:title, :description)
-		
 	end
 
 end 
