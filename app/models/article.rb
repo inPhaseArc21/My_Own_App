7b7592cc876e3190fd7a1733f8467799eb7cb2f6
Raj Silari <raj.silari@icloud.com>
2016-01-22 15:45:38 +1000

    Updating errors and creating name method

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
 	validates :title, presence: true, length: { minimum: 3, maximum: 50 }
 	validates :description, presence: true, length: { minimum: 30, maximum: 500 }
+	validates :name, presence: true, length: { minimum: 2, maximum: 20 }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117102252) do
+ActiveRecord::Schema.define(version: 20160122020124) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "crreated_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
